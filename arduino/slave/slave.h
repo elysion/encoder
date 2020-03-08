226ad8568832d200e217a186f03522f40c1b3ece
Miko Kiiski <miko.kiiski@gmail.com>
2020-03-08 20:39:25 +0200

    Remove unnecessary rgbw color function;
    The LEDs used in the project are rgb and thus this function is not
    needed (at least for this project).

--- arduino/slave/slave.h
+++ arduino/slave/slave.h
@@ -57,9 +57,6 @@ public:
   static uint32_t Color(uint8_t r, uint8_t g, uint8_t b) {
     return Adafruit_NeoPixel::Color(r, g, b);
   }
-  static uint32_t Color(uint8_t r, uint8_t g, uint8_t b, uint8_t w) {
-    return Adafruit_NeoPixel::Color(r, g, b, w);
-  }
 #endif
 
 private:
